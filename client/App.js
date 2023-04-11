c8b69f8c2a80b5d7e5da1d58bc193f0123e890a7
Hangning-Li <h623li@uwaterloo.ca>
2023-04-10 21:07:49 -0400

    second commit

--- client/App.js
+++ client/App.js
@@ -3,9 +3,36 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { addData2GoogleSheet } from './utils/addData2GoogleSheet';
 import { useEffect } from 'react';
 
+function updateSheet(data){
+  axios({
+    method: 'post',
+    url: 'http://10.0.2.2:8000/add_data',
+    data: JSON.stringify(data),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+    .then(function (response) {
+      alert("sheet updated");                
+    })
+    .catch(function (error) {
+        if (error.response) {
+            // Request made and server responded
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+    })
+}
 
 export default function App() {
-
+  <Button onPress={updateSheet(22)}></Button>
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express()
+const { google } = require("googleapis");
+
+app.post("/add_data", async (req, res) => {
+    try {
+        // Authorize with Google
+        const auth = new google.auth.GoogleAuth({
+            keyFile: '',
+            scopes: ['https://www.googleapis.com/auth/spreadsheets']
+        });
+
+        const authClient = await auth.getClient();
+        const sheets = google.sheets({
+            version: 'v4',
+            auth: authClient
+        });
+
+        // Set the spreadsheet ID and range
+        const spreadsheetId = '1xWPl2yj06fAG22O6Q3b2XwkTUEyIy5w4bsA30SX4Tuc';
+        const range = 'Sheet1!A1';
+
+        // Define the value to add
+        const value = data;
+
+        // Create the update request
+        const request = {
+            spreadsheetId: spreadsheetId,
+            range: range,
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: [
+                    [value]
+                ]
+            }
+        };
+
+        // Update the spreadsheet
+        const response = await sheets.spreadsheets.values.update(request);
+        console.log(`${response.data.updatedCells} cells updated.`);
+    } catch (error) {
+        console.error(`Error updating spreadsheet: ${error}`);
+    }
+
+    res.send("Successfully submitted! Thank you!");
+});
+dd
+app.listen(8080, (req, res) => console.log("running on 8080"));
